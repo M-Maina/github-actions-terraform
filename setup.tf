13814626e32d7447669a4902cc88e79171fa3563
Geoffrey M Maina <pythondeveloper21@gmail.com>
2024-03-19 05:32:17 -0700

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-weast-2"
 }
 
 module "wamahvpc" {
   source = "terraform-aws-modules/vpc/aws"
   cidr = "10.7.0.0/16"
+  azs             = ["eu-west-1a", "eu-west-1c"]
+  public_subnets  = ["10.7.100.0/24", "10.7.200.0/24"]
 }
